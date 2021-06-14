1cbb0b9e10a19c9e5ca8426b4c722db7bf9dc96c
reffj <julian.reff@zahnarztpraxis-dr-reff.de>
2021-06-14 12:53:17 +0200

    CalendarCreateViewModel Sec and mill
    Signed-off-by: reffj <julian.reff@zahnarztpraxis-dr-reff.de>

--- app/src/main/java/com/focustime/android/ui/calendar/create/CalendarCreateViewModel.java
+++ app/src/main/java/com/focustime/android/ui/calendar/create/CalendarCreateViewModel.java
@@ -32,6 +32,8 @@ public class CalendarCreateViewModel extends AndroidViewModel {
         beginTime.set(Calendar.DAY_OF_MONTH, Integer.parseInt(splitDate[2]));
         beginTime.set(Calendar.HOUR, d.getStartHour());
         beginTime.set(Calendar.MINUTE, d.getStartMinute());
+        beginTime.set(Calendar.SECOND, 0);
+        beginTime.set(Calendar.MILLISECOND, 0);
 
         Calendar endTime = Calendar.getInstance();
         endTime.set(Calendar.YEAR, Integer.parseInt(splitDate[0]));
@@ -40,6 +42,8 @@ public class CalendarCreateViewModel extends AndroidViewModel {
         endTime.set(Calendar.HOUR, d.getStartHour());
         endTime.set(Calendar.MINUTE, d.getStartMinute());
         endTime.add(Calendar.MINUTE, d.getDuration());
+        endTime.set(Calendar.SECOND, 0);
+        endTime.set(Calendar.MILLISECOND, 0);
         System.out.println(d.getDuration());
         System.out.println("Inserting in API (Begin End Diff): " + beginTime.getTimeInMillis() + " " + endTime.getTimeInMillis() + " Diff:" + (endTime.getTimeInMillis()-beginTime.getTimeInMillis()));
         FocusTime f = new FocusTime(d.getTitle(), beginTime, endTime);
